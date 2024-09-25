4f2865d1d9ec3b20dd5f08aca7ae5138b5cab6fe
Erick Tostes <117785300+ErickTostes@users.noreply.github.com>
2024-09-24 23:22:11 -0300

    Add files via upload

---
+++ ProductDetails.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './ProductDetails.css';
+
+const ProductDetails = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products/${id}`)
+      .then((response) => response.json())
+      .then((data) => setProduct(data));
+  }, [id]);
+
+  if (!product) {
+    return <div>Carregando...</div>;
+  }
+
+  return (
+    <div className="product-details-container">
+      <h1>{product.title}</h1>
+      <img src={product.image} alt={product.title} className="product-details-image" />
+      <p>{product.description}</p>
+      <p><strong>Preço:</strong> ${product.price}</p>
+      <p><strong>Categoria:</strong> {product.category}</p>
+    </div>
+  );
+};
+
+export default ProductDetails;
